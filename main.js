8f4ad6adeb3b83066da93a39aa12b6b5ab7c53da
Matheus <matheus.b.correia7@gmail.com>
2023-10-18 21:53:32 -0300

    Criando lista de teclas

--- main.js
+++ main.js
@@ -1,15 +1,7 @@
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
 function tocaSomPom() {
   document.querySelector("#som_tecla_pom").play();
 }
 
-function tocaSomClap() {
-  document.querySelector("#som_tecla_clap").play();
-}
-
-function tocaSomTim() {
-  document.querySelector("#som_tecla_tim").play();
-}
-
-document.querySelector(".tecla_pom").onclick = tocaSomPom;
-document.querySelector(".tecla_clap").onclick = tocaSomClap; //
-document.querySelector(".tecla_tim").onclick = tocaSomTim;
+listaDeTeclas[0].onclick = tocaSomPom;
