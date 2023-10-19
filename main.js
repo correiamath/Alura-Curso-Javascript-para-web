a07c8198679cb2c7b5594b9fb5ae25efd31097b1
Matheus <matheus.b.correia7@gmail.com>
2023-10-18 22:54:39 -0300

    Adicionando som as teclas

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const listaDeTeclas = document.querySelectorAll(".tecla");
+const listaDeSons = document.querySelectorAll("button");
 
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 
-listaDeTeclas[0].onclick = tocaSomPom;
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  contador += 1;
+}
